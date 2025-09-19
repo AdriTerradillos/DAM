e2bd6a69e955b87eb432ec5f1be28a74ec0f5e60
Adrián Terradillos <adrianterradillos025@gmail.com>
2025-09-19 14:37:01 +0000

    Nueva Actualización: agregando algunos casos prácticos relacionados con archivo Strings02.java

---
+++ Modulo_Programacion/Primera_Evaluacion/01Basicos/Strings02.java
@@ -0,0 +1,32 @@
+public class Strings02 {
+
+    public static void main(String[] args) {
+
+        /** Pautaspara trabajar con String
+         * 
+          1º- Generamos una o un par de variables 
+          2º- Generamos un mensaje con sysout y agregamos la(s) variable(s)
+         3º- Bualah 
+         */
+
+System.out.println("");
+
+
+         // EJERCICIO STRING BÁSICO
+
+        String primerString = "Mi nombre es Manolo y soy de Cuenca";
+        System.out.println("El usuario escribió lo siguiente: " + primerString + ".");
+
+
+System.out.println("");
+
+
+        // EJERCICIO STRING INTERMEDIO
+
+        String nombre = "Gustavo";
+        String argumento = "Estoy interesado por el puesto de Desarrollador de Aplicaciones Multiplataforma";
+        System.out.println("Mi nombre es " + nombre + ". Acabo de ver su oferta en el periódico. " + argumento + "."); 
+
+
+    }
+}
--- Modulo_Programacion/Primera_Evaluacion/04Scanner/ScannerCombinado03.java
+++ Modulo_Programacion/Primera_Evaluacion/04Scanner/ScannerCombinado03.java
@@ -16,7 +16,7 @@ public class ScannerCombinado03 {
      */
 
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
     
 
         // Objeto Scanner
