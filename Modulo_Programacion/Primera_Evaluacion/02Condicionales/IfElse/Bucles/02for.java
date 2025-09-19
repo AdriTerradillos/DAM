4aa32bd7b97231b42889e0df824b7d067d2daf9a
Adrián Terradillos <adrianterradillos025@gmail.com>
2025-09-19 08:45:22 +0000

    Nueva Actualización: 19/10/2025 -> agregando carpetas y reorganizando ejercicios con sus respectivas carpetas en ModuloProgramacion.

--- Modulo_Programacion/Primera_Evaluacion/02Condicionales/IfElse/Bucles/02for.java
+++
@@ -1,8 +0,0 @@
-// todos los numeros que existen desde el 1 al 100 (bucle for)
-
-public class 02BucleFor {
-
-
-
-    
-}
---
+++ Modulo_Programacion/Primera_Evaluacion/03bucles/02for.java
@@ -0,0 +1,20 @@
+// Ejercicio de clase: todos los numeros que existen desde el 1 al 100 (bucle for)
+
+public class 02BucleFor {
+
+    public static void main (String[] args) {
+    // generamos el bucle for con los siguientes datos...
+
+
+    /**
+     * INICIALIZACIÓN: Queremos incializar el bucle desde i = 3
+     * Queremos además, que el bucle sea menor (o igual) a 100
+     * Por último, vamos a ir saltando de 3 en 3 hasta llegar al 100
+     */
+
+    for (int = 3; i<= 100; i+=3) {
+        Systm.out.println(i);
+    }
+
+}
+}
