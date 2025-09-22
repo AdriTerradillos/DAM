e98a14dc393992f065020824e4f93b49f97a71c0
Adrián Terradillos <adrianterradillos025@gmail.com>
2025-09-22 11:26:28 +0000

    Nueva Actualización: agregando archivos prácticos a carpeta.

--- Modulo_Programacion/Primera_Evaluacion/Practica/operadoresAritmeticos02.java
+++ Modulo_Programacion/Primera_Evaluacion/Practica/operadoresAritmeticos02.java
@@ -4,7 +4,58 @@ public class operadoresAritmeticos02 {
 
     public static void main(String[] args) {
 
-        
+        /**
+         * === Operadores Aritméticos ===
+         --> Suma: ++
+         --> Resta: --
+         --> Multiplicación: *
+         --> División: /
+         --> Módulo: %
+         */
+
+
+         // Ejercicio 1 (suma y resta)
+
+         int numero1 = 12;
+         int numero2 = 7;
+
+         int suma = numero1 + numero2;
+         int resta = numero1 - numero2;
+
+         System.out.println("EL resultado de la suma es: " + suma + ".");
+         System.out.println("El resultado de la resta es: " + resta + ".");
+
+
+System.out.println("");
+
+
+         // Ejercicio 2 (multiplicación y división)
+
+         int numero3 = 48;
+         int numero4 = 2;
+
+         int multiplicacion = numero3 * numero4;
+         int division = numero3 / numero4;
+
+         System.out.println("El resultado de la multiplicación es: " + multiplicacion + ".");
+         System.out.println("El reultado de la división es: " + division + ".");
+
+
+System.out.println("");
+
+
+         // Ejercicio 3 (módulo o %)
+
+         int numero5 = 40;
+         int numero6 = 5;
+
+         int modulo = 40%2;
+
+         System.out.println("El resultado de este módulo es: " + modulo + ".");
+
+
+
+
     }
     
 }
---
+++ Modulo_Programacion/Primera_Evaluacion/Practica/sentenciasIfForWhile03.java
@@ -0,0 +1,52 @@
+package Primera_Evaluacion.Practica;
+
+public class sentenciasIfForWhile03 {
+    
+    public static void main (String[] args) {
+
+
+        /**
+         === Sentencias If - For - While
+
+          --> if: condicional
+          --> for: bucles
+          --> while: bucles
+
+         */
+
+
+
+         // Ejercicio 1 (condicional if)
+
+         double valor1 = 12.4;
+         int valor2 = 21;
+
+         if(valor1 > valor2) {
+
+            System.out.println("El número " + valor1 + "es mayor que el número " + valor2);
+
+
+          } else if(valor1 < valor2) {
+
+                System.out.println("EL número " + valor2 + " es mayor que el número " + valor1);
+        }
+
+
+
+        // Ejercicio 2 (bucle for)
+
+        private class ejercicio2 {
+        int i;
+
+        for (i = 0; i > 100; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+
+
+        // Ejercicio 3 (bucle while)
+            }
+            } 
+
