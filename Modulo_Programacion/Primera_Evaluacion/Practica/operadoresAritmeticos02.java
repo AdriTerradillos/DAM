bf9c1b8bf6d0392508e09ea103205cdfe5a86351
Adrián Terradillos <adrianterradillos025@gmail.com>
2025-09-22 11:02:23 +0000

    Nueva Actualización: agregando el ejercicio práctico numerosEnteros01.java

---
+++ Modulo_Programacion/Primera_Evaluacion/Practica/operadoresAritmeticos02.java
@@ -0,0 +1,10 @@
+package Primera_Evaluacion.Practica;
+
+public class operadoresAritmeticos02 {
+
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
---
+++ Modulo_Programacion/Primera_Evaluacion/Practica/variablesNumericas01.java
@@ -0,0 +1,47 @@
+package Primera_Evaluacion.Practica;
+
+public class variablesNumericas01 {
+    
+    /*
+     === NÚMEROS ENTEROS ===
+
+     --> int
+     --> byte
+     --> long
+     --> short
+
+     === NÚMEROS DECIMALES ===
+
+     --> float
+     --> double
+
+     */
+
+public static void main (String[] args) {
+
+    // *** Ejercicio 1: Variable con enteros ***
+
+    int edad = 32;
+    int pulgadas = 45;
+
+    System.out.println("Mauricio, el entrenador de baloncesto ¡es súper jóven! Tiene " + edad + " años.");
+    System.out.println("La televisión de Carla se ve grande. Tiene " + pulgadas + " pulgadas.");
+    System.out.println("Mauricio, socio de " + edad + " años, a comprado la televisión a Carla. Cuenta con " + pulgadas + " pulgadas.");
+
+
+    // *** Ejercicio 2: Variable con decimales ***
+
+    double metros = 189.3;
+    float temperatura = 25F;
+
+    System.out.println("Este edificio es el más alto de la ciudad. Tiene una altura de " + metros + " metros.");
+    System.out.println("¡Hace mucho calor en este restaurante! Oscilaremos los " + temperatura + " grados.");
+    System.out.println("Dentro de este edificio hay un restaurante con una temperatura que supera los " + temperatura + " grados.");
+
+    
+
+
+
+}
+
+}
