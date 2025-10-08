76b114f8a52eca128719ead8c9af55d2372a5ce4
AdriTerradillos <adrianterradillos025@gmail.com>
2025-10-08 20:36:18 +0200

    Nueva Actualización: agregando a Primera_Evaluacion/practicas/'boletin01' ejercicios restantes.

--- Modulo_Programacion/Primera_Evaluacion/Practicas/boletin02/practica02.java
+++ Modulo_Programacion/Primera_Evaluacion/Practicas/boletin02/practica02.java
@@ -3,39 +3,36 @@ package Primera_Evaluacion.Practicas.boletin02;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class practica01 {
-	
+public class practica02 {
 	
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
 		
-		System.out.println("Ingresa la primera palabra: ");
+		System.out.println(" Ingresa la primera palabra: ");
 		String palabra1 = teclado.nextLine();
 		
-		System.out.println("Ingresa la segunda palabra: ");
+		System.out.println(" Ingresa la segunda palabra: ");
 		String palabra2 = teclado.nextLine();
 		
 		System.out.println("Ingresa la tercera palabra: ");
 		String palabra3 = teclado.nextLine();
 		
 		
-		// guardamos las tres palabras dentro de un Array
-		String[] palabras = {palabra1, palabra2, palabra3 };
+		// de nuevo, guardamos las palabras dentro de un Array
+		String[] palabras = {palabra1, palabra2, palabra3};
 		
-		// ordenamos el array alfabéticamente
+		// ordnamos Array alfabéticamente
 		Arrays.sort(palabras);
 		
 		// mostramos resultado
-		System.out.println("\nPalabras ordenadas alfabéticamente: ");
 		
-		for(String p : palabras) {
-			System.out.println(p);
-			
+		System.out.println("\n Palabras ordenadas de la Z a la A: ");
+		
+		for (int i = palabras.length - 1; i >= 0; i--) {
+			System.out.println(palabras[i]);
 		}
-		
 		
 		teclado.close();
 	}
-	
 }
